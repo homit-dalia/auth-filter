dbb5d3c1bb5432def30b372d38251c0f25e7c8d3
homitdalia@gmail.com <hd431@banyan.cs.rutgers.edu>
2025-11-15 12:36:01 -0500

    sockperf bench m

--- scripts/run_sbench.sh
+++ scripts/run_sbench.sh
@@ -46,7 +46,7 @@ for sz in "${SIZES[@]}"; do
   if ! sockperf throughput \
          --client_ip "${CLIENT_BIND_IP}" \
          -i "${SERVER_IP}" \
-         --p 11111 \
+         -p 11111 \
          --msg-size "${sz}" \
          --time "${DURATION_SEC}" \
          --full-rtt \
